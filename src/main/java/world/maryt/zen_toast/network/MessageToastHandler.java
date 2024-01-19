b568b42943a5a7011e49dc118e277fac16983993
Isaac Thompson <risinginiris2017@gmail.com>
2024-01-19 19:36:10 +0800

    side only

--- src/main/java/world/maryt/zen_toast/network/MessageToastHandler.java
+++ src/main/java/world/maryt/zen_toast/network/MessageToastHandler.java
@@ -5,11 +5,13 @@ import net.minecraft.client.gui.toasts.IToast;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import world.maryt.zen_toast.ZenToast;
-import world.maryt.zen_toast.network.MessageToast;
 import world.maryt.zen_toast.toast.SimpleToast;
 
 public class MessageToastHandler implements IMessageHandler<MessageToast, IMessage> {
+    @SideOnly(Side.CLIENT)
     @Override
     public IMessage onMessage(MessageToast message, MessageContext ctx) {
         Minecraft.getMinecraft().addScheduledTask(() -> {
